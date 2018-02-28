27e8de27a25350acbc7d596acfac4f232982db96
Steve Tractenberg <steve@blacks4.com>
2018-02-28 00:07:01 -0500

    changed

--- aws_cli/recipes/default.rb
+++ aws_cli/recipes/default.rb
@@ -5,9 +5,6 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 package 'AWS Command Line Interface' do
-  source 'https://s3.amazonaws.com/aws-cli/AWSCLI64.msi '
-   action :install
+  source 'https://s3.amazonaws.com/aws-cli/AWSCLI64.msi'
+  action :install
 end
-
-
-#{}test
